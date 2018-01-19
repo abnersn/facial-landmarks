b82474205004a9009fcff626d15a3e1b3c63c05b
Abner <abnersousanascimento@gmail.com>
2018-01-19 16:40:30 -0300

    intensity data

--- train.py
+++ train.py
@@ -6,11 +6,13 @@ import cv2, dlib
 import modules.util as util
 from multiprocessing import Pool
 from modules.procrustes import calculate_procrustes, mean_of_shapes
+from scipy.spatial.distance import cdist as distance
+from imutils import resize
 
-
-IMAGES_PATH = './img_train'
+IMAGES_PATH = './img_test'
 ANNOTATIONS_PATH = './data'
 NUMBER_OF_TREES = 500
+NUMBER_OF_REFPOINTS = 400
 TREES_DEPTH = 3
 NUMBER_OF_REGRESSORS = 1
 SHRINKAGE_FACTOR = 0.01
@@ -24,11 +26,15 @@ print('reading annotations from disk')
 annotations = util.read_annotations(ANNOTATIONS_PATH)
 print('all data has been successfully loaded into memory')
 
+print('calculating mean of shapes...')
 annotations = calculate_procrustes(annotations)
 shapes_mean = mean_of_shapes(annotations)
 
+print('sorting reference points')
+radius = np.max(distance(shapes_mean, shapes_mean)) / 2
+points = util.sort_points(400, 0, radius)
 
-def calculate_estimation(item):
+def first_estimation(item):
     file_name, image = item
     faces = detector(image)
     if len(faces) > 0:
@@ -37,21 +43,37 @@ def calculate_estimation(item):
         middle = ((np.array(top_left) + np.array(bottom_right))
                   / 2) + np.array([-10, 40])
         scale = faces[0].width() * 0.3
-        estimation = (shapes_mean * scale) + middle
-        return (file_name, estimation)
+        return (file_name, {
+            'estimation': (shapes_mean * scale) + middle,
+            'reference_points': (points * scale) + middle
+        })
     else:
         print('no face detected on {}'.format(file_name))
         os.unlink(os.path.join(IMAGES_PATH, file_name))
-        return (file_name, [])
+        return (file_name, None)
 
 p = Pool(4)
-estimations = p.map(calculate_estimation, images.items())
+print('calculating estimations...')
+data = dict(p.map(first_estimation, images.items()))
 
-for file_name, estimation in estimations:
-    util.plot(images[file_name], estimation)
-    cv2.imshow('image', images[file_name])
-    cv2.waitKey(1000)
+# print('showing images...')
+# for file_name, information in data:
+#     image = images[file_name]
+#     util.plot(image, information['reference_points'])
+#     cv2.imshow('image', resize(image, width=400))
+#     cv2.waitKey(300)
 
+difference_data = {}
+intensity_data = {}
+for file_name, information in data.items():
+    image = images[file_name]
+    real_shape = annotations[file_name[:-4]] 
+    difference_data[file_name] = (real_shape - information['estimation'])
+    intensity_data[file_name] = []    
+    for point in information['reference_points']:
+        x = min(int(point[0]), image.shape[1] - 1)
+        y = min(int(point[1]), image.shape[0] - 1)
+        intensity_data[file_name].append(image.item(y, x))
+
+print(intensity_data)
 input('halt...')
-
-print(shapes_mean)
