086aad87e6f5c93ebfa77a1891b77f108d399199
Abner <abnersousanasicmento@gmail.com>
2019-03-17 12:01:42 -0300

    Remove unnecessary files

--- build_faulty.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python build_faulty_dataset.py datasets/training_150 datasets/training_2000 -p 20 -c 40
-python build_faulty_dataset.py datasets/training_150 datasets/training_2000 -p 20 -c 60
-# python build_faulty_dataset.py datasets/training_150 datasets/training_2000 -p 20 -c 80
